35f0ffb0a2ab0dc9d44ed7adcc06e4a5edc8b98e
veter984 <oleg_k2013@mail.ru>
2025-12-19 11:39:07 +0300

    Знакомство с PageObject2

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,5 +1,4 @@
 from playwright.sync_api import Page, expect
-import time
 from pages.base_page import BasePage
 
 
@@ -12,5 +11,5 @@ class DashboardPage(BasePage):
     def check_visible_dashboard_title(self):
        expect(self.dashboard_title).to_be_visible()
        expect(self.dashboard_title).to_have_text("Dashboard")
-       time.sleep(3)
+
 
