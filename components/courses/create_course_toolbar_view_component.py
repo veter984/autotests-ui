8b4cc5723045615a96f0efae13da0c35761714b3
veter984 <oleg_k2013@mail.ru>
2026-01-11 22:09:23 +0300

    pageactory

--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -14,9 +14,9 @@ class CreateCourseToolbarViewComponent(BaseComponent):
     def check_visible(self, is_create_course_disabled=True):
         expect(self.title).to_be_visible()
         expect(self.title).to_have_text('Create course')
-        if is_create_course_disabled == True:
+        if is_create_course_disabled:
             expect(self.create_course_button).to_be_disabled()
-        if is_create_course_disabled == False:
+        if not is_create_course_disabled:
             expect(self.create_course_button).to_be_enabled()
 
     def click_create_course_button(self):
