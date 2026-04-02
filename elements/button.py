a1509914f6d9b6b9f00a2d5faf7f6e8535458ae9
veter984 <oleg_k2013@mail.ru>
2026-04-02 21:04:37 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/button.py
+++ elements/button.py
@@ -2,6 +2,7 @@ from playwright.sync_api import expect
 import allure
 from elements.base_element import BaseElement
 from tools.logger import get_logger
+from ui_coverage_tool import ActionType
 
 logger = get_logger("BUTTON")
 
@@ -17,9 +18,15 @@ class Button(BaseElement):
             logger.info(step)
             expect(locator).to_be_enabled()
 
+        # После успешного check_enabled фикс покрытия как действие ENABLED
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+
+        # После успешного check_disabled фикс покрытия как действие DISABLED
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/textarea.py
+++ elements/textarea.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Locator, expect
 import allure
 from elements.base_element import BaseElement
 from tools.logger import get_logger
+from ui_coverage_tool import ActionType
 
 logger = get_logger("TEXTAREA")
 
@@ -14,6 +15,13 @@ class Textarea(BaseElement):
         # Получаем локатор textarea
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        # Переопределяем метод формирования XPath-селектора:
+        #  - сначала получаем общий селектор блока
+        #  - затем уточняем путь до самого <textarea>, добавляя '//textarea'
+        # Это нужно, чтобы трекер точно знал, с каким элементом шло взаимодействие.
+        return f'{super().get_raw_locator(**kwargs)}//textarea[1]'
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
@@ -21,9 +29,15 @@ class Textarea(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        # После успешного fill фиксируем покрытие как действие FILL
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        # Фиксируем в покрытии, что значение проверено — тип VALUE
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
